044812833071bfc1f70fe9b18ab252fafdfa160f
Avadhut Kesarkkar <avadhoot.kesarkar@gmail.com>
2020-06-19 20:52:48 +0530

    Add files via upload

---
+++ 1_Printer.py
@@ -0,0 +1,12 @@
+"""
+ Develop a simple printer
+ Your program must show the text from the output example
+"""
+
+print("I do love animals!")
+print("Start looking after animals")
+print("Deer looks fine")
+print("Bat looks happy")
+print("Lion looks healthy")
+
+print("My name is Avadhoot")
